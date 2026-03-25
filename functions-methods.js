de57b49eb6d0ee8630d9fab70efa2a5a993b3831
PatS4050 <patrick@straatman.eu>
2026-03-25 21:00:53 +0100

    opgaves methods gemaakt, krijg bij de laatste opdracht de value . niet goed

--- functions-methods.js
+++ functions-methods.js
@@ -9,8 +9,17 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+// const userInputEmail = prompt('Enter your email');
+// const userInputEmail = ["n.eeken@novi-education.nl", "t.mellink@novi.nl"]
+// console.log(userInputEmail);
 
 
+function getEmailDomain(userInputEmail) {
+    const emailIndex = userInputEmail.indexOf('@');
+    return userInputEmail.substring(emailIndex + 1);
+}
+
+console.log(getEmailDomain("user@novi.nl"))
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,7 +29,25 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+// functie vergelijkt of ze domein novi herkent en geeft Medewerker
+// functie vergelijkt of ze eeen novi-education domein heeft - early return en geeft Student
+// al het overige is extern
 
+
+function typeOfEmail(userInputEmail) {
+    const emailIndex = userInputEmail.indexOf("@");
+    const emailDomain = userInputEmail.substring(emailIndex + 1);
+//     return userInputEmail.substring(emailIndex + 1);
+    switch (emailDomain) {
+        case "novi.nl":
+            return "Medewerker";
+        case "novi-education":
+            return "Student";
+        default:
+            return "Extern"
+    }
+}
+console.log(typeOfEmail("user@gmail"));
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +61,18 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity (userInputEmail) {
+    const emailIndex = userInputEmail.indexOf("@");
+    if (userInputEmail.includes(",")){
+        return userInputEmail = false }
+    if (userInputEmail.lastIndexOf(".") === userInputEmail.length) {
+        return userInputEmail = false }
+    else if (userInputEmail.includes("@")){
+        return userInputEmail = true }
+    else {
+        return userInputEmail = false }
+}
+console.log(checkEmailValidity("user@novi.nl"))
+
+// De punt notatie op regel 69 werkt niet Grrrrrr
